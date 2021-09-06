e314f6996f2c30f121afc5d6838734c817712753
Juan Davalos <jcharlied13@gmail.com>
2021-09-05 18:07:58 -0700

    First commit to the prototype TTRPG game
    Trying a prototype that might change as time goes along. Currently in the movement phase of the project.

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public float lerpSpeed = 5.0f;
+    public GameObject player;
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        lerpCamera();
+    }
+
+    void lerpCamera()
+    {
+        Vector3 desiredPos = player.transform.position + offset;
+        transform.position = Vector3.Lerp(transform.position, desiredPos, Time.deltaTime * lerpSpeed);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject travelDistance;
+    public Collider planeCollider;
+    public Vector3 movementLimits;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandlePlayerMovement();
+        CheckBoundaries();
+    }
+    
+    // Handle the players lateral movements
+    void HandlePlayerMovement()
+    {
+        // Player movement is done on a square by square movement
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += Vector3.forward;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position += Vector3.back;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += Vector3.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += Vector3.right;
+        }
+    }
+    void CheckBoundaries()
+    {
+        // If the player ever gets past the boundaries return it by one block
+        if(transform.position.x > movementLimits.x)
+        {
+            transform.position += Vector3.left;
+        }
+        else if(transform.position.x < -movementLimits.x)
+        {
+            transform.position += Vector3.right;
+        }
+        if(transform.position.z > movementLimits.z)
+        {
+            transform.position += Vector3.back;
+        }
+        else if(transform.position.z < -movementLimits.z)
+        {
+            transform.position += Vector3.forward;
+        }
+    }
+}
